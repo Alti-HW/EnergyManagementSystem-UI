66518bf25b5ea849444c3906e2cc6719f268f212
Kalva Sulochana <skalva@altimetrik.com>
2025-02-10 15:29:50 +0530

    added Roles UI

--- src/containers/user_managements/roles/roles_table.tsx
+++ src/containers/user_managements/roles/roles_table.tsx
@@ -5,7 +5,9 @@ import {
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
+  TablePagination,
   TableRow,
   Tooltip,
   tooltipClasses,
@@ -14,6 +16,8 @@ import {
 import CloseIcon from "@mui/icons-material/Close";
 import CheckIcon from "@mui/icons-material/Check";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { useState } from "react";
+import PopoverWithMenu from "./popoverwithMenu";
 
 const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
@@ -24,7 +28,20 @@ const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
   },
 });
 
-const FixedFirstColumnTable = ({ roles, permissions }: any) => {
+const RolesTable = ({ roles, permissions, onRoleEdit, onRoleDelete }: any) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <TableContainer component={Paper} sx={{ overflow: "auto" }}>
       <Table aria-label="simple table">
@@ -47,55 +64,82 @@ const FixedFirstColumnTable = ({ roles, permissions }: any) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {roles?.map((role: any, index: number) => (
-            <TableRow key={index}>
-              <TableCell
-                sx={{
-                  position: "sticky",
-                  left: 0,
-                  backgroundColor: "#fff",
-                  zIndex: 1,
-                  width: "25%",
-                }}
-              >
-                {role.roleName}
-                <CustomWidthTooltip
-                  title={role.description}
-                  arrow
-                  placement="right"
+          {roles
+            ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((role: any, index: number) => (
+              <TableRow key={index}>
+                <TableCell
+                  sx={{
+                    position: "sticky",
+                    left: 0,
+                    backgroundColor: "#fff",
+                    zIndex: 1,
+                    width: "25%",
+                  }}
                 >
-                  <InfoOutlinedIcon
-                    sx={{
-                      width: "16px",
-                      height: "16px",
-                      position: "absolute",
-                      top: "50%",
-                      transform: "translateY(-50%)",
-                      marginLeft: "5px",
-                      cursor: "pointer",
-                    }}
+                  {role?.name}
+                  <CustomWidthTooltip
+                    title={role?.description}
+                    arrow
+                    placement="right"
+                  >
+                    <InfoOutlinedIcon
+                      sx={{
+                        width: "16px",
+                        height: "16px",
+                        position: "absolute",
+                        top: "50%",
+                        transform: "translateY(-50%)",
+                        marginLeft: "5px",
+                        cursor: "pointer",
+                      }}
+                    />
+                  </CustomWidthTooltip>
+                  <PopoverWithMenu
+                    onRoleEdit={onRoleEdit}
+                    onRoleDelete={onRoleDelete}
+                    index={index}
                   />
-                </CustomWidthTooltip>
-              </TableCell>
-              {permissions?.map((permission: string) => (
-                <TableCell>
-                  {role?.permissions?.indexOf(permission) !== -1 ? (
-                    <CheckIcon
-                      sx={{ width: "20px", height: "20px", color: "#5EB082" }}
-                    />
-                  ) : (
-                    <CloseIcon
-                      sx={{ width: "20px", height: "20px", color: "#e64c4c" }}
-                    />
-                  )}
                 </TableCell>
-              ))}
-            </TableRow>
-          ))}
+                {permissions?.map((permission: string) => (
+                  <TableCell>
+                    {role?.permissions?.indexOf(permission) !== -1 ? (
+                      <CheckIcon
+                        sx={{ width: "20px", height: "20px", color: "#5EB082" }}
+                      />
+                    ) : (
+                      <CloseIcon
+                        sx={{ width: "20px", height: "20px", color: "#e64c4c" }}
+                      />
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell
+              colSpan={permissions?.length + 1}
+              sx={{
+                padding: 0.5,
+              }}
+            >
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 15]}
+                component="div"
+                count={roles.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </TableCell>
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
 };
 
-export default FixedFirstColumnTable;
+export default RolesTable;
